ebc221dc6c9816911895df825a9806f9d2ee9d55
Moibe <moi_estrello@hotmail.com>
2024-12-28 17:07:53 -0600

    Api selection fixes

--- globales.py
+++ globales.py
@@ -5,14 +5,14 @@ version = "4.8.12"
 env = "dev"
 aplicacion = "astroblend-dev"
 
-seleccion_api = "eligeGratisOCosto" #eligeGratisOCosto , eligeAOB o eligeGratisOCosto
+seleccion_api = "eligeAOB" #eligeGratisOCosto , eligeAOB o eligeGratisOCosto
 max_size = 20
 #Quota o Costo
 api_zero = ("Moibe/image-blend", "gratis")
 api_cost = ("Moibe/image-blend", "costo")
 #A o B
 api_a = ("Moibe/image-blend", "gratis")
-api_b = ("Moibe/image/blend", "gratis")
+api_b = ("Moibe/image-blend", "gratis")
 #Gratis o Costo
 api_gratis = ("Moibe/image-blend", "gratis")
 api_costo = ("Moibe/image-blend", "costo")
