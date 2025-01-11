e8a8e0f01d2d4c8595d8e9f6573a2e4801828a34
Moibe <moi_estrello@hotmail.com>
2025-01-10 18:32:03 -0600

    Fix code route at keepalive.sh

--- keepalive.sh
+++ keepalive.sh
@@ -7,7 +7,7 @@ if [ -z "$pid" ]; then
   echo "$timestamp1 - ATENCIÓN: No se encontró ningún proceso escuchando en el puerto 7820. Reactivando aplicación."
   #Reiniciando proceso
   cd
-  cd code/gradio-standalone-do/
+  cd code/ocean-devo-palette/
   source venv/bin/activate
   python app.py &
   nuevo_proceso=$(pgrep -f "python app.py")
